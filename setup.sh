e5011a7a9a2555f67e4ab5da3156e1c09250841a
dsx724 <da@lessconfused.com>
2019-09-07 03:55:18 -0400

    add mesa dependencies for kmscube

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 SOURCE_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 cd $SOURCE_DIR
 
-apt-get install -y kmscube iperf network-manager
+apt-get install -y kmscube iperf network-manager mesa-utils-extra
 sed -i s/^#dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d/ /boot/config.txt
 systemctl disable dhcpcd
 sed -i s/^managed=false/managed=true/ /etc/NetworkManager/NetworkManager.conf
